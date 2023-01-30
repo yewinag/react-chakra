6758b4037b8674cd559b1535f53118464db07195
yewinag <yewinaung831@gmail.com>
2023-01-30 20:03:56 +0700

    add header

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './CTThemeEditor';
+export * from './DesktopNav';
+export * from './Logo';
+export * from './MobileNav';
+export * from './navData';
+export * from './TextUnderline';
---
+++ src/components/navData.ts
@@ -0,0 +1,25 @@
+export interface NavItem {
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
+}
+
+export const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: 'About',
+    href: '/',
+  },
+  {
+    label: 'Blogs',
+    href: '/',
+  },
+  {
+    label: 'History',
+    href: '/',
+  },
+  {
+    label: 'Projects',
+    href: '/',
+  },
+];
